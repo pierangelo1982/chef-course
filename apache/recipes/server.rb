5849ebf00c9a3d534f8d7603d84d028177fa47ab
vagrant <vagrant@localhost.localdomain>
2017-07-07 12:22:34 +0000

    refactored server.rb with template index.html

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -3,11 +3,21 @@ package 'git'
 package 'httpd'
 
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, World!</h1>
-  <h3>#{node['hostname']}</h3>
-  <h3>#{node['ipaddress']}</h3>
-"
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello, World!</h1>
+#  <h3>#{node['hostname']}</h3>
+#  <h3>#{node['ipaddress']}</h3>
+#"
+#end
+
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+    :name => 'pierangelo',
+    :email => 'pierangelo1982@gmail.com'
+  )
+  action :create
 end
 
 service 'httpd' do
