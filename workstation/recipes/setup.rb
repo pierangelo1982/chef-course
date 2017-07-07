3bd7c8b672a9712fb8abd85c8ceb509470f06db9
vagrant <vagrant@localhost.localdomain>
2017-07-07 17:22:24 +0000

    add user and group in setup.rb in workstation

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -24,3 +24,16 @@ template '/etc/motd' do
   )
   action :create
 end
+
+
+user 'user1' do
+  comment 'user1'
+  uid '123'
+  home '/home/user1'
+  shell '/bin/bash'
+end
+
+
+group 'admins' do
+  members 'user1'
+end
