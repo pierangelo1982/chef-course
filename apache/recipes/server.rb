1eb9d73166f6c8d46fb7d474b7e23386d363ba7b
vagrant <vagrant@localhost.localdomain>
2017-07-07 16:17:24 +0000

    se veral example of script bash and not wth execute ecc... in server.rb apache

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -20,15 +20,51 @@ template '/var/www/html/index.html' do
   action :create
 end
 
+
 cookbook_file '/var/www/html/test.html' do
   source 'test.html'
   #action :create
+end
+
+
+bash "inline script" do
+  user "root"
+  code "mkdir -p /var/www/mysites && chown -R apache /var/www/mysite"
+  #not_if '[ -d /var/www/mysites ]'
+  not_if do
+    File.directory?('/var/www/mysites/')
+  end
+end
+
+
+execute "run a script" do
+  user "root"
+  command <<-EOH
+  mkdir -p /var/www/mysites/ /
+  chown -R apache /var/www/mysites
+  EOH
+end
+
+
+#execute "run script" do
+#  user "root"
+#  command './myscript.sh'
+#end
+
+
+directory "/var/www/mysites" do
+  owner "apache"
+  recursive true
 end
 
 remote_file '/var/www/html/pierangelo.jpg' do
   source 'https://avatars3.githubusercontent.com/u/4020950'
 end
 
+
 service 'httpd' do
   action :start
 end
+
+
+
