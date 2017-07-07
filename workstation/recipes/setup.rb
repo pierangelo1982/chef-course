f2a8f8ac428d886fc9181df114e14206cbc2e5fb
vagrant <vagrant@localhost.localdomain>
2017-07-07 06:29:11 +0000

    add data in workstation recipe setup for /etc/motd

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -5,7 +5,12 @@ end
 package 'tree'
 
 file '/etc/motd' do
-  content 'This server is property of a funny guy named pierangelo orizio'
+  content 'This server is property of a funny guy named pierangelo orizio
+  HOSTNAME: localhost.localdomain
+  IPADDRESS: 10.0.2.15
+  CPU: 3100.000
+  MEMORY: 500780
+'
   action :create
   owner 'root'
   group 'root'
