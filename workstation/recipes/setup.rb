ec312026ba298032e9d9e6e4fefde340f77c3e5f
vagrant <vagrant@localhost.localdomain>
2017-07-07 11:57:54 +0000

    refactor motd file with template

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -4,14 +4,20 @@ end
 
 package 'tree'
 
-file '/etc/motd' do
-  content 'This server is property of a funny guy named pierangelo orizio
-  HOSTNAME: localhost.localdomain
-  IPADDRESS: 10.0.2.15
-  CPU: 3100.000
-  MEMORY: 500780
-'
+#file '/etc/motd' do
+#  content 'This server is property of a funny guy named pierangelo orizio
+#  HOSTNAME: localhost.localdomain
+#  IPADDRESS: 10.0.2.15
+#  CPU: 3100.000
+#  MEMORY: 500780
+#'
+#  action :create
+#  owner 'root'
+#  group 'root'
+#end
+
+
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
-  owner 'root'
-  group 'root'
 end
